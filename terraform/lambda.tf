ba545961c3ac2f598cb508d27cfc916d30b7e6dd
Samuel Antônio Rabelo Pereira <samuel.pereira@vitaebrasil.com.br>
2025-08-25 10:39:09 -0300

    correção do zip

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,35 +1,41 @@
 resource "aws_iam_role" "lambda_exec" {
-name = "lambda-stop-role"
-assume_role_policy = jsonencode({
-Version = "2012-10-17"
-Statement = [{
-Action = "sts:AssumeRole"
-Principal = { Service = "lambda.amazonaws.com" }
-Effect = "Allow"
-}]
-})
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-role = aws_iam_role.lambda_exec.name
-policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-
-resource "aws_iam_role_policy_attachment" "lambda_stop_ec2" {
-role = aws_iam_role.lambda_exec.name
-policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
-
 
 resource "aws_lambda_function" "stop_resources" {
-function_name = "stop-resources"
-role = aws_iam_role.lambda_exec.arn
-handler = "handler.lambda_handler"
-runtime = "python3.12"
+  function_name = "stop-resources"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.12"
 
+  # >>> ZIP será gerado no pipeline e salvo em terraform/lambda.zip
+  filename         = "${path.module}/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+}
 
-filename = "${path.module}/../lambda/lambda.zip"
-source_code_hash = filebase64sha256("${path.module}/../lambda/lambda.zip")
+# Permissão para o SNS disparar o Lambda
+resource "aws_lambda_permission" "allow_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.stop_resources.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.budget_notification.arn
 }
