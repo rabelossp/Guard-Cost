d8ba4209ed1e8cf84dd50cd1b1d1246890ff1858
Samuel Antônio Rabelo Pereira <samuel.pereira@vitaebrasil.com.br>
2025-08-26 10:53:24 -0300

    ajuste do .tf

--- terraform/budget.tf
+++ terraform/budget.tf
@@ -1,37 +1,37 @@
 resource "aws_budgets_budget" "monthly" {
-name = "cost-guard"
-budget_type = "COST"
-limit_amount = "0.01"
-limit_unit = "USD"
-time_unit = "MONTHLY"
+  name         = "cost-guard"
+  budget_type  = "COST"
+  limit_amount = "0.01"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
 
 
-notification {
-comparison_operator = "GREATER_THAN"
-threshold = 0.01
-threshold_type = "ABSOLUTE_VALUE"
-notification_type = "FORECASTED"
-subscriber_sns_topic_arns = [aws_sns_topic.alerts.arn]
-}
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 0.01
+    threshold_type            = "ABSOLUTE_VALUE"
+    notification_type         = "FORECASTED"
+    subscriber_sns_topic_arns = [aws_sns_topic.alerts.arn]
+  }
 }
 
 
 resource "aws_sns_topic" "alerts" {
-name = "cost-guard-alerts"
+  name = "cost-guard-alerts"
 }
 
 
 resource "aws_sns_topic_subscription" "lambda_sub" {
-topic_arn = aws_sns_topic.alerts.arn
-protocol = "lambda"
-endpoint = aws_lambda_function.stop_resources.arn
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.stop_resources.arn
 }
 
 
 resource "aws_lambda_permission" "sns_invoke" {
-statement_id = "AllowExecutionFromSNS"
-action = "lambda:InvokeFunction"
-function_name = aws_lambda_function.stop_resources.function_name
-principal = "sns.amazonaws.com"
-source_arn = aws_sns_topic.alerts.arn
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.stop_resources.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.alerts.arn
 }
